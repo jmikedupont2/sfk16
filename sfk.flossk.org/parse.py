473570ef7075bef5c16a48ddf3fe4ab5ad8134cb
mike dupont <Jmikedupont2@gmail.com>
2016-10-16 10:13:52 +0200

    test

--- sfk.flossk.org/parse.py
+++ sfk.flossk.org/parse.py
@@ -1,4 +1,8 @@
 from bs4 import BeautifulSoup
+import re
+import unicodedata
+
+
 f = open ('index.html')
 html_doc=""
 for l in f.readlines():
@@ -9,11 +13,19 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 
 for d in soup.find_all('div', class_='speaker'):
     #print "Speaker" + d.h4.text
-    d.id_="Speaker_" + d.h4.text
 
+    t=d.h4.text
+    t=re.sub(r'\s+','_', t)
+    t=unicodedata.normalize('NFKD', t).encode('ascii', 'ignore')
+    d['id']="Speaker_" + t.decode('utf-8')
+    
 for d in soup.find_all('div', class_='event'):
     #print "Talk"+ d.h3.text
-    d.id_="Talk_" + d.h3.text
+    t=d.h3.text
+    t=re.sub(r'\s+','_', t)
+    t=unicodedata.normalize('NFKD', t).encode('ascii', 'ignore')
+    d['id']="Talk_" + t.decode('utf-8')
+
     
 
 print(soup.prettify())
